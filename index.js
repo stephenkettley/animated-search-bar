0d60731e41913fead4cb8a919b2f20c5f142819e
Stephen Kettley <kettleystephen@gmail.com>
2023-09-26 16:56:29 +0200

    update functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+const searchBarContainer = document.getElementById("search-container");
+
+searchBarContainer.addEventListener("click", () => {
+  console.log("clicked");
+  searchBarContainer.style.backgroundColor = "#ffffff";
+});
