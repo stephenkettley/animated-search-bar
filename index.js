a33938b2c8da710d7e26fb411377f66c58770d5d
Stephen Kettley <kettleystephen@gmail.com>
2023-09-26 17:10:24 +0200

    finalise functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const searchBarContainer = document.getElementById("search-container");
+const magnifierElement = document.querySelector(".magnifier");
 
-searchBarContainer.addEventListener("click", () => {
-  console.log("clicked");
-  searchBarContainer.style.backgroundColor = "#ffffff";
+magnifierElement.addEventListener("click", () => {
+  searchBarContainer.classList.toggle("active");
+  searchBarContainer.style.backgroundColor = "#f0f8ff";
 });
